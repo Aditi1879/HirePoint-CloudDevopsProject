1eabc8403b67292459a4332cab89264004af18e7
Aditi1879 <aditiyadav3010@gmail.com>
2026-08-10 23:00:40 +0530

    Fix company listing cache issue

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -1,26 +1,36 @@
-import { setCompanies } from '@/redux/companySlice'
-import { COMPANY_API_END_POINT } from '@/utils/constant'
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { setCompanies } from '@/redux/companySlice';
+import { COMPANY_API_END_POINT } from '@/utils/constant';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-export const useGetAllCompanies = (companyId) => {
+export const useGetAllCompanies = () => {
     const dispatch = useDispatch();
-    useEffect(() =>{
-        const fetchCompanies = async () => {
-            try{
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get`, {withCredentials:true});
-                if(res.data.success){
-                    dispatch(setCompanies(res.data.companies));
 
+    useEffect(() => {
+        const fetchCompanies = async () => {
+            try {
+                const res = await axios.get(
+                    `${COMPANY_API_END_POINT}/get?refresh=${Date.now()}`,
+                    {
+                        withCredentials: true,
+                        headers: {
+                            'Cache-Control': 'no-cache',
+                            'Pragma': 'no-cache'
+                        }
+                    }
+                );
+
+                if (res.data.success) {
+                    dispatch(setCompanies(res.data.companies));
                 }
-            } catch(error){
+            } catch (error) {
                 console.log(error);
             }
-        }
+        };
+
         fetchCompanies();
-    }, [])
-  
-  
-}
-export default useGetAllCompanies 
+    }, [dispatch]);
+};
+
+export default useGetAllCompanies;
